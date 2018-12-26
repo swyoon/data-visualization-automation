6785c4fabd5535f22f32b67f016b6466efdfacbf
Sangwoong Yoon <sangwoong24yoon@gmail.com>
2018-12-26 14:07:35 +0900

    second commit

--- 0_data_gen.py
+++ 0_data_gen.py
@@ -1,10 +1,13 @@
+"""
+generates example dataset
+"""
 import numpy as np
 import pandas as pd
 
 N = 1000
 D = 1
 X = np.random.randn(N, D)
-Y = X ** 2 + np.random.randn(N,D) * 0.1
+Y = X ** 3 + np.random.randn(N,D) * 0.1
 data = np.hstack([X, Y])
 df = pd.DataFrame(data, columns=['X', 'Y'])
 df.to_csv('example_data.csv', index=None)
